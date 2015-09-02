c2f0ac907bb3d65a342a3ce4a121a2aba0fcce1e
madhuranga sampath bandara <bandaraonline@gmail.com>
2015-09-02 17:08:22 +0530

    netty sender changes

--- src/main/java/org/wso2/carbon/gateway/internal/mediation/camel/CamelMediationComponent.java
+++ src/main/java/org/wso2/carbon/gateway/internal/mediation/camel/CamelMediationComponent.java
@@ -40,11 +40,11 @@ public class CamelMediationComponent extends DefaultComponent {
         return endpoint;
     }
 
-    //when the camel component (wso2-gw) starts this method will be called initially
+/*    //when the camel component (wso2-gw) starts this method will be called initially
     @Override
     protected void doStart() throws Exception {
         super.doStart();
         //start netty transport from here get a CamelMediationEngine object and set it to engine.
         //this.engine = (CamelMediationEngine) new POCController().startPOCController();
-    }
+    }*/
 }
